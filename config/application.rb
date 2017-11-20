0f53b1ff3cfc2ed23e092e8244ea2aa223063f26
Pichenko <dasha.dementova@gmail.com>
2017-11-20 14:13:40 +0200

    load_lib_fix

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Analyzer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << "#{Rails.root}/lib"
     # config.exceptions_app = self.routes
   end
 end
