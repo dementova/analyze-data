18462580004e07ab58bff11d0910289af6ab3d2b
Dementova Dasha <you@example.com>
2017-12-12 08:58:27 +0200

    chart_controller_fix

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,9 +4,4 @@ class ChartsController < ApplicationController
   def index
     @result = ChartManager.generate FILE
   end
-
-  private
-  def _file_params
-  	params.require(:file).permit(:report)
-  end
 end
